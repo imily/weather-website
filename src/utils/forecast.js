dda0eacaa224fcffaf7ef1570827e41b97e146d7
Imily_Huang <imily_huang@mail.chungyo.net>
2023-10-15 16:58:31 +0800

    add humidity

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -8,7 +8,10 @@ const forecast = (latitude, longitude, callback) => {
     } else if (body.error) {
       callback('Unable to find location!', undefined);
     } else {
-      const data = `${body.current.weather_descriptions[0]} throughout the day. It is currently ${body.current.temperature} degrees out. It feels like ${body.current.feelslike} degrees out.`
+      const data = `${body.current.weather_descriptions[0]} throughout the day.
+        It is currently ${body.current.temperature} degrees out.
+        It feels like ${body.current.feelslike} degrees out.
+        The humidity is ${body.current.humidity} period`
       callback(undefined, data);
     }
   })
